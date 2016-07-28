7906a08a595b5fcb0c5b754de8321ad6833b07af
Thomas Miller <thomasmillergb@outlook.com>
2016-07-28 13:06:04 +0100

    add questions content

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,11 @@ var quiz =
 	q1: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan', value: true}, {text: 'Guarantee'}]},
 		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit', value: true}]},
 		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution', value: true}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}],
-	q2: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan', value: true}, {text: 'Guarantee'}]},
-		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit', value: true}]},
-		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution', value: true}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}],
-	q3: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan', value: true}, {text: 'Guarantee'}]},
-		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit', value: true}]},
-		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution', value: true}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}]
+	q2: [{type: 'radio', question: 'Which bank account has an interest-free overdraft?', options: [{text: 'Savings account'}, {text: 'Student account', value: true}, {text: 'Current account'}]},
+		{type: 'radio', question: 'Darren has won the lottery and has received £100,000,000. Which account do you think would be best for him to deposit his money?', options: [{text: 'Savings account', value: true}, {text: 'Student account', value: false}, {text: 'Current account'}]},
+		{type: 'radio', question: 'Which account would be most for day-to-day costs for a non-student?', options: [{text: 'Savings account'}, {text: 'Student account', value: false}, {text: 'Current account', value: true}]}],
+	q3: [{type: 'radio', question: 'Which of the below is not a type of savings account?', options: [{text: '	Fixed-Rate Bonds'}, {text: '	Money Market Account', value: true}, {text: 'Cash ISAs'}]},
+		{type: 'radio', question: 'Which type of savings account offers the highest interest rate?', options: [{text: '	Fixed-Rate Bonds'}, {text: '	Money Market Account'}, {text: '', value: true}]}]
 };
 
 var resoureces = [
