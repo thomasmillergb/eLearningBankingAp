0e4705e4e7829a0413bdd66e4dd6f27053a9041e
Thomas Miller <thomasmillergb@outlook.com>
2016-07-28 12:42:34 +0100

    add content

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,24 @@ var contentResource = require('./videoText.json');
 
 var quiz =
 {
-	q1: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan'}, {text: 'Guarantee'}]},
-		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit'}]},
-		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution'}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}]
+	q1: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan', value: true}, {text: 'Guarantee'}]},
+		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit', value: true}]},
+		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution', value: true}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}]
+	q2: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan', value: true}, {text: 'Guarantee'}]},
+		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit', value: true}]},
+		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution', value: true}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}]
+	q3: [{type: 'radio', question: 'Money you borrow from a bank is a:', options: [{text: 'Deposit'}, {text: 'Loan', value: true}, {text: 'Guarantee'}]},
+		{type: 'radio', question: 'Money you store at the bank is:', options: [{text: 'Loan'}, {text: 'Interest'}, {text: 'Deposit', value: true}]},
+		{type: 'radio', question: 'What is a bank?', options: [{text: 'Financial Institution', value: true}, {text: 'Charity'}, {text: 'Non-for-profit Organisation'}]}]
 };
 
 var resoureces = [
 	{uri: '/', name: 'index', extra: {title: 'Express'}},
 	{
 		uri: '/modles', name: 'modle-screen', extra: {title: 'Express'}, sub: [
-		{uri: '/vid1', name: 'modle', extra: {quiz: quiz.q1/*{q:quiz.q1}*/, video: 'www.youtube.com', videosrc: 'https://www.youtube.Wcom/embed/CqD3hnjZBTM', content: contentResource.vid1}},
-		{uri: '/vid2', name: 'modle', extra: {video: 'www.youtube.com', videosrc: 'https://www.youtube.com/embed/smHxeRdf7oI', content: contentResource.vid2}},
-		{uri: '/vid3', name: 'modle', extra: {video: 'www.youtube.com', videosrc: 'https://www.youtube.com/embed/ftDQOV6K158"', content: contentResource.vid3}},
+		{uri: '/vid1', name: 'modle', extra: {quiz: quiz.q1, video: 'www.youtube.com', videosrc: 'https://www.youtube.com/embed/CqD3hnjZBTM', content: contentResource.vid1}},
+		{uri: '/vid2', name: 'modle', extra: {quiz: quiz.q2, video: 'www.youtube.com', videosrc: 'https://www.youtube.com/embed/smHxeRdf7oI', content: contentResource.vid2}},
+		{uri: '/vid3', name: 'modle', extra: {quiz: quiz.q3, video: 'www.youtube.com', videosrc: 'https://www.youtube.com/embed/ftDQOV6K158"', content: contentResource.vid3}},
 	]
 	},
 	{uri: '/page', name: 'page1', extra: {}}
